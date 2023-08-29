576adef324c0fd233b1582d458968e81ef93ab33
Wangda Tan <wangda@apache.org>
2023-08-29 14:04:59 -0700

    upgraded waii-sdk-js version

--- src/waii-cli.ts
+++ src/waii-cli.ts
@@ -88,14 +88,7 @@ const initialize = async () => {
         throw Error("Please provide your Waii API key in the config file: " + configPath)
     }
 
-    WAII.initialize(config.url, config.apiKey);
-    let result = await WAII.Database.getConnections({});
-
-    if (result.default_db_connection_key) {
-        await WAII.Database.activateConnection(result.default_db_connection_key);
-    } else if (result.connectors && result.connectors.length > 0) {
-        await WAII.Database.activateConnection(result.connectors[0].key);
-    }
+    await WAII.initialize(config.url, config.apiKey);
 }
 
 const main = async () => {
