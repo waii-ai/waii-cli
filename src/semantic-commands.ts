6194c3ac2fadd3366c6ea6bab341c3bd6c333e58
Gunther Hagleitner <gunther@Gunthers-MBP.lan>
2023-09-07 23:37:14 -0700

    add importer

--- src/semantic-commands.ts
+++ src/semantic-commands.ts
@@ -1,19 +1,21 @@
 import WAII from 'waii-sdk-js'
 import { CmdParams } from './cmd-line-parser';
 import { SemanticStatement } from "waii-sdk-js/dist/clients/semantic-context/src/SemanticContext";
-import {Table} from "console-table-printer";
-import {IIndexable} from "./query-commands";
+import { Table } from "console-table-printer";
+import { IIndexable } from "./query-commands";
 
 const printStatements = (statements?: SemanticStatement[]) => {
     if (!statements) {
         console.log("No statements found.");
         return;
     }
-    const p = new Table({ columns: [
+    const p = new Table({
+        columns: [
             { name: 'scope', alignment: 'left', maxLen: 10, minLen: 1 },
-            { name: 'statement', alignment: 'left', minLen: 40, maxLen: 40}
+            { name: 'statement', alignment: 'left', minLen: 40, maxLen: 40 }
             // { name: 'labels', alignment: 'left' }
-        ], rowSeparator: true });
+        ], rowSeparator: true
+    });
     for (const stmt of statements) {
         p.addRow({
             scope: stmt.scope,
@@ -35,6 +37,39 @@ const contextList = async (params: CmdParams) => {
             printStatements(result.semantic_context);
         }
     }
+}
+
+const contextImport = async (params: CmdParams) => {
+    let context = await WAII.SemanticContext.getSemanticContext();
+    let importContext = JSON.parse(params.input);
+    let totalCounter = 0;
+    let importCounter = 0;
+
+    for (const stmt of importContext.semantic_context) {
+        let found = false;
+        totalCounter = totalCounter + 1;
+        if (context.semantic_context) {
+            for (const current of context.semantic_context) {
+                if (current.scope === stmt.scope && current.statement === stmt.statement) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                let newStatement: SemanticStatement = new SemanticStatement(
+                    stmt.scope,
+                    stmt.statement
+                );
+                let result = await WAII.SemanticContext.modifySemanticContext(
+                    {
+                        updated: [newStatement]
+                    }
+                );
+                importCounter = importCounter + 1;
+            }
+        }
+    }
+    console.log("Read ", totalCounter, " statement(s), ", "imported ", importCounter, " statement(s)");
 }
 
 const contextAdd = async (params: CmdParams) => {
@@ -77,7 +112,8 @@ const contextDelete = async (params: CmdParams) => {
 const semanticCommands = {
     list: contextList,
     add: contextAdd,
-    delete: contextDelete
+    delete: contextDelete,
+    import: contextImport
 };
 
 export { semanticCommands };
